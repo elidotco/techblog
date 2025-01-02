63c5fc6d9518dff4ed697e4ced6263cb03254591
Ebenezer Ametepeh <eddiefestde@gmail.com>
2025-01-02 00:27:49 +0000

    i made the herosection responsive

--- frontend/app/sections/HeroSection.jsx
+++ frontend/app/sections/HeroSection.jsx
@@ -1,13 +1,19 @@
+import {
+  ArrowTurnRightUpIcon,
+  ArrowUpRightIcon,
+} from "@heroicons/react/20/solid";
 import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="flex justify-between border-b border-dark-100 ">
+    <section className="flex lg:flex-row flex-col justify-between border-b border-dark-100 ">
       {/* Intro and description */}
-      <div className="text-dark-100 w-full pt-32 sm:w-3/5 border-r-[0.5px] border-dark-100">
-        <div className="flex flex-col gap-5 pb-16  pl-32 pr-28 justify-center border-b-[0.5px] border-dark-100">
-          <p className="text-2xl ">Your Journey to Tomorrow Begins Here</p>
-          <h1 className="text-[3.5vw] text-white font-medium">
+      <div className="text-dark-100 w-full  pt-10 xl:pt-32 lg:w-3/5 lg:border-r-[0.5px] border-dark-100">
+        <div className="flex flex-col gap-5 pb-16 pl-5 md:px-20  xl:pl-32 pr-5 xl:pr-28 justify-center border-b-[0.5px] border-dark-100">
+          <p className="text-normal xl:text-2xl ">
+            Your Journey to Tomorrow Begins Here
+          </p>
+          <h1 className="lg:text-[3.5vw] text-[6vw] text-white font-medium">
             Explore the Frontiers of Artificial Intelligence
           </h1>
           <p className="text-md">
@@ -17,7 +23,7 @@ const HeroSection = () => {
             AI.
           </p>
         </div>
-        <div className="flex pl-32  text-white justify-between w-full">
+        <div className="flex pl-5 md:px-20 xl:pl-32  text-white justify-between w-full">
           <StCa num={300} desc="Resources Avilable" p={true} />
           <StCa num={100} desc="Resources Avilable" />
           <StCa num={2300} desc="Resources Avilable" b={true} />
@@ -25,13 +31,26 @@ const HeroSection = () => {
       </div>
       {/* Intro and description */}
       {/* Image and the other stuff at the right hand side*/}
-      <div className="w-2/5  h-full p-0">
+      <div className="lg:w-2/5  relative   p-0">
         <img
           src="/hero.png"
           alt="hero-image"
-          className="w-1/2 h-3/5 top-0 opacity-70 right-0 "
+          className="xl:w-2/3 w-full  h-full top-0 opacity-70 right-0 "
         />
-        <div className="w-full h-1/2"></div>
+        <div className="w-full h-3/5 xl:h-1/2 flex items-center lg:block absolute  bottom-0 px-5 xl:pl-20 2xl:pl-28  flex-col ">
+          <img src="/round.png" alt="hero-image" className="pb-5 sm:w-2/5" />
+          <h3 className="text-white font-normal text-xl">
+            Explore 1000+ resources
+          </h3>
+          <p className=" py-5 text-sm text-gray-700">
+            Over 1,000 articles on emerging tech trends and breakthroughs.
+          </p>
+
+          <button className="border-[0.5px] mt-4 border-dark-100 text-gray-700 flex items-center gap-1  px-6 py-2 rounded-md">
+            Explore Resources{" "}
+            <ArrowUpRightIcon className="h-5 w-5 text-yellow-700" />
+          </button>
+        </div>
       </div>
       {/* Image and the other stuff at the right hand side*/}
     </section>
@@ -43,13 +62,13 @@ export default HeroSection;
 export const StCa = ({ b, num, desc, p }) => {
   return (
     <div
-      className={`py-10 ${
+      className={` py-5 xl:py-10 ${
         b ? "" : "border-r"
       } border-dark-100 flex items-start flex-col ${
-        p ? "" : "pl-20"
+        p ? "" : "xl:pl-20 pl-5 "
       }  w-1/3  "`}
     >
-      <h2 className="text-4xl font-medium">
+      <h2 className="text-xl 2xl:text-4xl font-medium">
         {num}
         <span className="ml--2 text-yellow-800 ">+</span>
       </h2>
