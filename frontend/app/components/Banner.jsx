96129ef59b33592e1ec1ea2ef5e72e8fd9d52c41
Ebenezer Ametepeh <eddiefestde@gmail.com>
2025-01-13 15:33:25 +0000

    changes to the footer

--- frontend/app/components/Banner.jsx
+++ frontend/app/components/Banner.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-const Banner = ({ data }) => {
+const Banner = ({ data, type }) => {
   return (
-    <div className="bg-dark-600 w-full py-10 lg:px-20 md:px-10 px-5 2xl:px-32">
+    <div
+      className={`${
+        type
+          ? "flex flex-row flex-wrap lg:flex-col"
+          : "bg-dark-600 w-full py-10 lg:px-20 md:px-10 px-5 2xl:px-32"
+      }`}
+    >
       <div className="p-2 bg-dark-500 text-sm rounded-sm mb-5  w-fit">
         {data.title}
       </div>
-      <p className="md:text-4xl text-2xl text-white">{data.excerpt}</p>
+      <p
+        className={`md:text-4xl text-2xl text-white ${
+          type ? "w-full lg:flex-1" : ""
+        }`}
+      >
+        {data.excerpt}
+      </p>
     </div>
   );
 };
--- frontend/app/sections/Footer.jsx
+++ frontend/app/sections/Footer.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Banner } from "../components";
 
 const Footer = () => {
   return (
     <footer className="">
       {/* Top section  */}
-      <section className="px-5 md:px-10 lg:px-20 2xl:px-32 bg-dark-700 min-h-72"></section>
+      <section className="px-5 md:px-10 lg:px-20 2xl:px-32 bg-dark-700 min-h-72">
+        <div className="flex gap-x-10 items-center py-20">
+          <img src="/logo.png" className="w-20 h-20 " />
+          <div className="flex flex-col">
+            <Banner
+              data={{
+                title: "Learn, Connect, and Innovate",
+                excerpt: "Be Part of the Future Tech Revolution",
+              }}
+              type={1}
+            />
+            <p className="text-gray-800 text-sm py-4">
+              Immerse yourself in the world of future technology. Explore our
+              comprehensive resources, connect with fellow tech enthusiasts, and
+              drive innovation in the industry. Join a dynamic community of
+              forward-thinkers.
+            </p>
+          </div>
+        </div>
+      </section>
       {/* Top section  */}
     </footer>
   );
