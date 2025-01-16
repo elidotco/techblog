44a460987871cf0202f736fd582d9888df7a69de
Ebenezer Ametepeh <eddiefestde@gmail.com>
2025-01-16 23:35:25 +0000

    ok

--- frontend/app/news/page.jsx
+++ frontend/app/news/page.jsx
@@ -4,15 +4,14 @@ const Home = () => {
   return (
     <section>
       {/* Title */}
-      <div className="py-20 px-5 md:10 lg:px-20 2xl:px-32">
-        Today's Headlines: Stay <br />
-        Informed{" "}
-        <span className="text-sm text-gray-">
+      <div className="py-20 px-5 md:10 lg:px-20 flex text-7xl items-end 2xl:px-32">
+        Today's Headlines: Stay Informed{" "}
+        <div className="text-base -ml-96 text-gray-600 ">
           {" "}
           Explore the latest news from around the world. We bring you
           up-to-the-minute updates on the most significant events, trends, and
           stories. Discover the world through our news coverage.
-        </span>
+        </div>
       </div>
       {/* Title */}
     </section>
